a359fc6bbc21971081012f045b2e5c93a283c412
unknown <sapokk.pinga@gmail.com>
2024-09-12 11:34:15 -0300

    ATT

---
+++ aula/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Aula Next</h1>
+      </main>
+    </>
+  );
+}
---
+++ aula/src/pages/sobre.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+import Head from "next/head";
+
+export default function Sobre() {
+  return (
+    <>
+      <Head>
+        <title>Sobre</title>
+      </Head>
+      <Header />
+      <main>
+        <h1>Sobre</h1>
+      </main>
+    </>
+  );
+}
